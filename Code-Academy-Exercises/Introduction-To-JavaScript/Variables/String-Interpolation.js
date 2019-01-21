41ed1442334e0a9951a173ada95701aed8a32bb0
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 13:47:15 -0700

    String Interpolation

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,4 @@
+//I learned how to use the templates literals to concatenate strings and string variables in a single sentence.
+let myName = 'Diana';
+let myCity = 'Denver';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
