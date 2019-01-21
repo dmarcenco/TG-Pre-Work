c65f9c8eb9d598e82dd09ff33f9d3da429e8de22
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 10:40:41 -0700

    Variables

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,3 @@
+/*variables allow us to label data and store it in the computer memory
+the variables can be created, store data by assigning a value to the variable, or reassign a value by changingig its value
+we also can refference the variable(use the information that is stored in the variable)*/
