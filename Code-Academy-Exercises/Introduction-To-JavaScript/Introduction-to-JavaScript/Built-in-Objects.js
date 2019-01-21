9a444cd3db0d9e1bbd2baa0e4737979551551db4
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 01:36:59 -0700

    Built in Objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,4 @@
+//I learned about the built-in Math object and its methods and how to use them in the code
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
