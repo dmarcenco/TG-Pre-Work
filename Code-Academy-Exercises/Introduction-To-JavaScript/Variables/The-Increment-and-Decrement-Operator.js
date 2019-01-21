4ca27f541688e998058838e791e4c6a3146d3fc7
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 13:09:13 -0700

    Increment and Decrement Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/The-Increment-and-Decrement-Operator.js
@@ -0,0 +1,5 @@
+//I learned how to use the increment ++ and decrement -- operators to update the value of a variable by 1, increase or decrease respectively
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
