51028dfe635f9dc10da0fffd1e991908cba552a7
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 00:55:35 -0700

    Arithmetic Operators

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,5 @@
+//I learned about the arithmetic operator +, -, /, *, % as well as how to perform them on numbers
+console.log(33 + 3.5);
+console.log(2019 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
