76d1b28342c3af0720af8f7e6a81b47406305a46
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 00:46:42 -0700

    Data Types

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,5 @@
+//I learned that Javascript has 7 data types, as well as how to print strings and numbers into the console
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
