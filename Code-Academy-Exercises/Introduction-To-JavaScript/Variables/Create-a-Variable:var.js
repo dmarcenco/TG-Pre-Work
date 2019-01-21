3f8018ef92282178d5cb9f19e475e4ee3cd07f34
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 12:13:46 -0700

    Create a variable: var

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,6 @@
+/*to create a variable we use the var keyword to declare the variable with the variable name(which should not be a keyword or start with a number)
+ use the assignment operator to assign it a value then initialize it with a value*/
+
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood + ' ' + numOfSlices + ' slices');
