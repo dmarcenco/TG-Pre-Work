1907b75132217292f1063999687076745ee821ce
dmarcenco <42789122+dmarcenco@users.noreply.github.com>
2019-01-21 01:06:51 -0700

    Properties

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,2 @@
+//I learned how to use the dot (.) operator together with the length property with the strings to print the number of characters in the specified string
+console.log('Teaching the world how to code'.length);
